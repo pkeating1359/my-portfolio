eba7fe6e1f7cccb533e1d332ba06aeed3e19fb86
Paul Keating <pkeating1359@gmail.com>
2017-09-28 13:42:46 -0400

    Add SNS and error exception code to the portfolio update code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,27 +6,42 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    # TODO implement
 
-    s3 = boto3.resource('s3')
-    ## Required if encryption support is not added by boto3
-    ## s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    # SNS Definitions
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:975855450853:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.cnimbus.biz')
-    build_bucket = s3.Bucket('portfoliobuild.cnimbus.biz')
+    try:
+        # S3 resource, bucket Definitions and variables
+        s3 = boto3.resource('s3')
+        ## Required if encryption support is not added by boto3
+        ## s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.cnimbus.biz')
+        build_bucket = s3.Bucket('portfoliobuild.cnimbus.biz')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-      for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        #portfolio_bucket.upload_fileobj(obj,nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        # Python StringIO function and file definition
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print "Portfolio updated!"
+        #Parsing the zip file in the target
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+          for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            #portfolio_bucket.upload_fileobj(obj,nm)
+            portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    return 'Hello from Lambda'
+        # Output for log
+        print "Portfolio updated!"
+
+        # SNS Publish action to notify us in email
+        topic.publish(Subject="Portfolio deployed",Message="Portfolio deployment was successfull!")
+
+    except:
+        # SNS Publish action to warn us in email
+        topic.publish(Subject="Portfolio deploy failed",Message="Portfolio deployment was NOT successfull!")
+
+    return 'Portfolio Lambda action completed'
     
