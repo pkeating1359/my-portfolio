77feec36264a22264d5746f450f2fe294e743626
Paul Keating <pkeating1359@gmail.com>
2017-09-29 12:32:10 -0400

    Fixed codepipeline support

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
         if job:
             for artifact in job["data"]["inputArtifacts"]:
                 if artifact["name"] == "MyAppBuild":
-                    location = artifact["location"]["s3location"]
+                    location = artifact["location"]["s3Location"]
 
         print "Building portfolio from " + str(location)
 
